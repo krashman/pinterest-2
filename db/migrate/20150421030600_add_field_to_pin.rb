8596c7e3e5e40526e3c37430c5c9f3b279a7a955
Leandro Tk <leandro.kinoshita@codus.com>
2015-04-21 00:09:39 -0300

    Generated migration to add user id to pins table

---
+++ db/migrate/20150421030600_add_field_to_pin.rb
@@ -0,0 +1,5 @@
+class AddFieldToPin < ActiveRecord::Migration
+  def change
+  	add_column :pins, :user_id, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150421025748) do
+ActiveRecord::Schema.define(version: 20150421030600) do
 
   create_table "pins", force: :cascade do |t|
-    t.string "title"
-    t.text   "description"
+    t.string  "title"
+    t.text    "description"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
