e1e89da66dfd040055dc0d2869c56e7fe782397f
Leandro Tk <leandro.kinoshita@codus.com>
2015-04-21 22:27:15 -0300

    Fixed papelclip to pin

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,6 +1,6 @@
 class Pin < ActiveRecord::Base
 	belongs_to :user
 
-	has_attached_file :avatar, styles: { medium: "300x300>" }
-  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
+	has_attached_file :image, styles: { medium: "300x300>" }
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
